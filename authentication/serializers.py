26fba5b0d788f5c6fb7f18b41e5fe2b5e9593a43
ibrahim77gh <syedibrahim4091@gmail.com>
2025-08-26 16:56:34 +0500

    Modified email templates and added  groups in auth/users/me API

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,12 +1,22 @@
 from djoser.serializers import UserSerializer as BaseUserSerializer, UserCreateSerializer as BaseUserCreateSerializer
 from rest_framework import serializers
 from .models import CustomUser
+from django.contrib.auth.models import Group
 
 
 class UserCreateSerializer(BaseUserCreateSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
         fields = ['id', 'password',
                   'email', 'first_name', 'last_name']
+        
+        def create(self, validated_data):
+            # Create user first
+            user = super().create(validated_data)
+            # Get or create client_user group
+            client_group, _ = Group.objects.get_or_create(name="client_user")
+            # Assign the group
+            user.groups.add(client_group)
+            return user
 
 class UserSerializer(BaseUserSerializer):
     class Meta(BaseUserSerializer.Meta):
@@ -14,7 +24,14 @@ class UserSerializer(BaseUserSerializer):
 
 
 class UserSerializer(serializers.ModelSerializer):
+    groups = serializers.SerializerMethodField()
     class Meta:
         model = CustomUser
-        fields = ['id', 'email', 'first_name', 'last_name', 'password', 'is_active', 'is_staff', 'is_superuser']
+        fields = ['id', 'email', 'first_name', 'last_name', 'password', 'is_active', 'is_staff', 'is_superuser', 'groups']
         extra_kwargs = {'password': {'write_only': True}}
+
+    def get_groups(self, obj):
+        """
+        Returns a list of group names for the user object.
+        """
+        return [group.name for group in obj.groups.all()]
