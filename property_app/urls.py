661140a8e892118993c5eb805dea2bd50863ff2f
ibrahim77gh <syedibrahim4091@gmail.com>
2025-09-06 11:11:08 +0500

    Created properties APIs

--- property_app/urls.py
+++ property_app/urls.py
@@ -1,7 +1,9 @@
-from property_app.views import CampaignSubmissionViewSet
+from property_app.views import CampaignSubmissionViewSet, PropertyViewSet, PropertyGroupViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'campaigns', CampaignSubmissionViewSet, basename='campaign')
+router.register(r'properties', PropertyViewSet, basename='property')
+router.register(r'property-groups', PropertyGroupViewSet, basename='property-group')
 
 urlpatterns = router.urls
